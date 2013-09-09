59cd63ff5f78f190fefd52c00688cd189bc14d9a
Oskar Jarczyk <oskar.jarczyk@pjwstk.edu.pl>
2013-09-09 03:09:45 +0200

    experiment on counting commits

--- iterate.py
+++ iterate.py
@@ -1,22 +1,29 @@
 import sys, datetime, time
-from pygithub3 import Github, exceptions
+from github import Github
 import csv
 
 def getrepos(username):
-    user_repos = gh.repos.list(username).all()
+    user_repos = gh.get_user(username).get_repos()
     for user_repo in user_repos:
-        print user_repo
-        #getcommits(username, user_repo)
-        outputter.writerow([username,(str(user_repo).strip('<Repo (')).strip(')>')])
+        print user_repo.name
+        getcommits(username, user_repo.name, user_repo)
+        #outputter.writerow([username,(str(user_repo).strip('<Repo (')).strip(')>')])
 
-def getcommits(username, user_repo):
+def getcommits(username, repo_name, user_repo):
     #gh.repos.commits.list(user=username, repo=user_repo)
-    for user_commit in gh.repos.commits.list(user=username, repo=user_repo):
-        print user_commit
-        #gives error 404.. time to move to https://github.com/jacquev6/PyGithub instead of pygithub3
+    user_commits = user_repo.get_commits()
+    i = 0
+    for commit in user_commits:
+        if (commit.committer.name == username):
+            i = i + 1
+            print commit + ' belongs to ' + username
+    languages = user_repo.get_languages()
+    for language in languages:
+        print language
+        outputter.writerow([username,repo_name,i,language])
 
 pass_string = open('pass.txt', 'r').read()
-gh = Github(user='wikiteams',login='wikiteams',password=pass_string)
+gh = Github('wikiteams',pass_string)
 
 with open('top-users-final.csv', 'rb') as source_csvfile:
     usersReader = csv.reader(source_csvfile, delimiter=',', quotechar='\'')
